22eb72a4b8921c63e594c33a88030b30d2320d4d
hubble_dj_home_server <makscum@gmail.com>
2025-03-16 03:42:24 +0300

    update DB class

--- modules/GlobalModules/DataBases.py
+++ modules/GlobalModules/DataBases.py
@@ -15,10 +15,11 @@ class DB:
         self.table_name = table_name
         self.columns = columns
 
-        self.create_db()
+        self.create_db_and_table()
     
-    def create_db(self) -> None:
-        """Создает базу данных"""
+    
+    def create_db_and_table(self) -> None:
+        """Создает базу данных и таблицу"""
         
         def get_column_crete_info(column: dict) -> str:
             """Строка с созданием колонки"""
@@ -43,42 +44,68 @@ class DB:
         asyncio.run(self.run_query(query))
     
     
-    def create_table(self) -> None:
-        """Создает таблицу"""
-        
-        pass
-    
-    
-    def add_values(self, columns: list, values: list):
+    def add_values(self, data: tuple[dict[str, str|int|float]]) -> None:
         """Добавляет записи в таблицу"""
         
-        pass
+        first_row_keys = list(data[0].keys())
+        
+        for row in data:
+            row_keys = frozenset(row.keys())
+            if len(frozenset(first_row_keys) ^ row_keys) > 0:
+                raise ValueError((
+                    'Разное количество параметров в строках. '
+                    f'''Первая строка: {', '.join(first_row_keys)}. '''
+                    f'''Другая: {', '.join(row_keys)}'''
+                ))
+            
+        query = f'''insert into {self.table_name} values({', '.join([f':{column}' for column in first_row_keys])})'''
+        
+        asyncio.run(self.run_query(
+            query=query,
+            data=data
+        ))
     
     
     def update_values(self, new_values: dict, condition: str) -> None:
         """Обновляет данные в таблице"""
         
-        pass
+        set_clause = ', '.join([f'''{column} = ?''' for column in new_values.keys()])
+        query = f'''update {self.table_name} set {set_clause} where {condition}'''
+        values = tuple(new_values.values())
+        
+        asyncio.run(self.run_query(
+            query=query,
+            values=values
+        ))
     
     
     def get_values(self, columns_name: list, condition: str) -> list:
         """Достает записи из таблицы"""
         
-        pass
+        query = f'''select {', '.join(columns_name)} from {self.table_name} where {condition}'''
+        
+        return asyncio.run(self.run_query(query=query))
     
     
     def drop_values(self, condition: str) -> None:
         """Удаляет записи"""
         
-        pass
+        query=f'''delete from {self.table_name} where {condition}'''
+        
+        asyncio.run(self.run_query(query=query))
     
     
-    async def run_query(self, query: str) -> Any:
+    async def run_query(self, query: str, data: tuple[dict[str, str|int|float]]=None, values: tuple[int|str|float]=None) -> Any:
         """Запрос в базу данных"""
         
         with sq.connect(self.path) as connect:
-            return connect.cursor().execute(query).fetchall()
-        
+            if data:
+                return connect.cursor().executemany(query, data).fetchall()
+            elif values:
+                return connect.cursor().execute(query, values).fetchall()
+            else:
+                return connect.cursor().execute(query).fetchall()
+
 
 if __name__ == '__main__':
     import json
@@ -86,8 +113,30 @@ if __name__ == '__main__':
         db_info = json.load(f)
         
     for table in db_info:
-        DB(
+        db = DB(
             path=f'''/home/hubble_dj/hubble_assistant_bot/databases/global_db/{table['name']}.db''',
             table_name=table['name'],
             columns=table['columns']
         )
+        
+        if db.table_name == 'text':
+            db.add_values(
+                data=(
+                    {
+                        'name': 'лол',
+                        'text': 'Рандомный текст',
+                        'group_in': '[]',
+                        'group_not_in': '[]'
+                    },
+                )
+            )
+
+            db.drop_values(condition='name=1')
+            
+            db.update_values(
+                new_values={'group_in': '1'},
+                condition='name="test"'
+            )
+            
+            print(db.get_values(columns_name=['group_in'], condition='name="валера"'))
+        
